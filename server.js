404c0e557682126dcb0ba4adb1484236c692fd03
Bolade Akinniyi <boladebode@gmail.com>
2022-08-04 21:39:00 +0100

    methods, data added

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
+var express = require('express');
+var serveStatic = require("serve-static")
+var path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
